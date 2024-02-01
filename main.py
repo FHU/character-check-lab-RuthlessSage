d86301fc2c0055ec1f7235c5926478d064d1a125
Sager Blackman <157738272+RuthlessSage@users.noreply.github.com>
2024-02-01 12:41:52 -0600

    Happy Birthday main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   pass
+   
+    char = word[index]
+
+    if char.isalpha():
+        return "letter"
+    elif char.isdigit():
+        return "digit"
+    elif char.isspace():
+        return "white space"
+    else:
+        return "unknown"
+
+if __name__ == '__main__': 
+    print(check_character('happy birthday', 2))
+    print(check_character('happy birthday', 5))
+    print(check_character('happy birthday 2 you', 15))
+    print(check_character('happy birthday!', 14))
 
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
